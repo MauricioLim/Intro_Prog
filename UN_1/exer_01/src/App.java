6485baee1f0ecf20d4be5207bd0f1f5d868619da
Maurício Dill de Lima <mdlima@furb.br>
2025-08-06 20:52:23 -0300

    first files

---
+++ UN_1/exer_01/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+    public static void teste(){
+        System.out.println("Hello, World!");
+    }
+    public static void main(String[] args) throws Exception {
+        App.teste();
+    }
+    
+}
+
