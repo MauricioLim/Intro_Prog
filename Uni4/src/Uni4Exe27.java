ba0643ac9a2ceb50e4f7119f057350e010d5faf7
Mauricio <mauriciodlima22@gmail.com>
2025-09-24 21:37:52 -0300

    adding the last exe

--- Uni4/src/Uni4Exe27.java
+++ Uni4/src/Uni4Exe27.java
@@ -6,6 +6,7 @@ public class Uni4Exe27 {
 
     public Uni4Exe27(){
         entrada();
+        verifica();
     }
 
     public void entrada(){
@@ -22,4 +23,46 @@ public class Uni4Exe27 {
         this.minSai = teclado.nextInt();
     }
 
+    public void verifica() {
+        if (!(horaCheg < 0 || horaCheg > 23 || horaSai < 0 || horaSai > 23 ||  minCheg < 0 ||    minCheg > 59 || minSai < 0 || minSai > 59)) {
+            valor();
+        } else{
+            System.out.println("Horario não permitido");
+        }
+    }
+
+    public void valor(){
+        int chegadaMin = this.horaCheg * 60 + this.minCheg;
+        int saidaMin = this.horaSai * 60 + this.minCheg;
+        int tempoTotal = saidaMin - chegadaMin;
+
+        int horas = tempoTotal / 60;
+        int minutosRestantes = tempoTotal % 60;
+
+        if (minutosRestantes < 30) {
+            if (horas > 0 && horas <= 2) {
+                System.out.printf("Preço cobrado = R$%.2f", (horas * 5));
+            } else if (horas > 2 && horas <= 4){
+                System.out.printf("Preço cobrado = R$%.2f", (horas * 7.5));
+            } else if (horas > 4){
+                System.out.printf("Preço cobrado = R$%.2f", (horas * 10));
+            }
+        } else if (minutosRestantes >= 30){
+            if (horas > 0 && horas <= 2) {
+                System.out.printf("Preço cobrado = R$%.2f", ((horas  + 1)* 5));
+            } else if (horas > 2 && horas <= 4){
+                System.out.printf("Preço cobrado = R$%.2f", ((horas  + 1) * 7.5));
+            } else if (horas > 4){
+                System.out.printf("Preço cobrado = R$%.2f", ((horas  + 1) * 10));
+            }
+        } else if (horas > 0 && minutosRestantes > 0){
+            System.out.printf("Preço cobrado = R$5");
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        new Uni4Exe27();
+        
+    }
 }
